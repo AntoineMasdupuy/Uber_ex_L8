07c46e503151cafeb5022bc5775b477d42c01e2f
AntoineMasdupuy <sba25162@student.cct.ie>
2026-05-17 21:28:42 +0100

    uber9
    uber9

--- uber.py
+++ uber.py
@@ -4,22 +4,17 @@ import numpy as np
 
 st.title("Uber Pickups in New York")
 
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-st.title("Uber Pickups in New York")
-
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-	'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-    
-def load_data(nrows):
-        data=pd.read_csv(DATA_URL, nrows=nrows)
-        lowercaser=lambda x:str(x).lower()
-        data.rename(lowercase, axis="columns", inplace=True)
-        data[DATE_COLUMN]=pd.to_datetime(data[DATE_COLUMN])
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-data_load_stste=st.text("Loading Data...")
-data=load_data(10000)
-data_load_state.textr("Loading Data Done!")
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text("Loading Data...")
+data = load_data(10000)
+data_load_state.text("Loading Data Done!")
